940ea28f5f528fe44c32a3118fd6a604012d1e06
Dwi Siswanto <me@dw1.io>
2022-12-14 08:45:24 +0700

    spec: update teler.Condition.Element type

--- condition.go
+++ condition.go
@@ -5,7 +5,7 @@ import "github.com/kitabisa/teler-waf/request"
 type Condition struct {
     // Element is the request element to match.
     // These element are defined in the request.Element type.
-    Element int
+    Element request.Element
 
     // Pattern is the regular expression to match against the element.
     Pattern string
