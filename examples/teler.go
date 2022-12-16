a57414c3e0006c94dc9d18a93e2acd7a9f917b7d
Dwi Siswanto <me@dw1.io>
2022-12-16 10:12:45 +0700

    add examples

---
+++ examples/teler.go
@@ -0,0 +1,45 @@
+package examples
+
+import (
+	"net/http"
+
+	"github.com/kitabisa/teler-waf"
+	"github.com/kitabisa/teler-waf/request"
+	"github.com/kitabisa/teler-waf/threat"
+)
+
+var myHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("hello world"))
+})
+
+func main() {
+	telerMiddleware := teler.New(teler.Options{
+		Excludes: []teler.Exclude{
+			threat.BadReferrer,
+			threat.BadCrawler,
+		},
+		Whitelists: []string{
+			`(curl|Go-http-client|okhttp)/*`,
+			`^/wp-login\.php`,
+			`https?:\/\/www\.facebook\.com`,
+			`192\.168\.0\.1`,
+		},
+		Customs: []teler.Rule{
+			{
+				Name:      "Log4j Attack",
+				Condition: "or",
+				Rules: []teler.Condition{
+					{
+						Element: request.Path,
+						Pattern: `\$\{.*:\/\/.*\/?\w+?\}`,
+					},
+				},
+			},
+		},
+		LogFile:   "/var/log/teler/teler.log",
+		LogRotate: true,
+	})
+
+	app := telerMiddleware.Handler(myHandler)
+	http.ListenAndServe("127.0.0.1:3000", app)
+}
