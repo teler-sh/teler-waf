1d7fcb7e3d18c21bd6bd22e31313162740c6e3f7
Dwi Siswanto <me@dw1.io>
2022-12-22 07:36:12 +0700

    spec: moving threat miscs to specific files

---
+++ threat/var.go
@@ -0,0 +1,11 @@
+package threat
+
+var str = map[Threat]string{
+	Undefined:           "Undefined",
+	CommonWebAttack:     "CommonWebAttack",
+	CVE:                 "CVE",
+	BadIPAddress:        "BadIPAddress",
+	BadReferrer:         "BadReferrer",
+	BadCrawler:          "BadCrawler",
+	DirectoryBruteforce: "DirectoryBruteforce",
+}
