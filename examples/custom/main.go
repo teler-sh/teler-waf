04769b941d1fc16c1268f341310ad80e5f071105
Dwi Siswanto <me@dw1.io>
2022-12-24 09:09:58 +0700

    spec: adding Method type

--- examples/custom/main.go
+++ examples/custom/main.go
@@ -30,7 +30,11 @@ func main() {
 				Condition: "or",
 				Rules: []teler.Condition{
 					{
+						Method:  request.GET,
 						Element: request.Path,
+						// you can use request.Any: it useful when you want to
+						// match against multiple elements of the request at once,
+						// rather than just a single element.
 						Pattern: `\$\{.*:\/\/.*\/?\w+?\}`,
 					},
 				},
