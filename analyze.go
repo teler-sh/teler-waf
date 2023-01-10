e0110773bba82f066363b074bb6d038ad2b173df
Dwi Siswanto <me@dw1.io>
2023-01-10 19:50:22 +0700

    refactor(BadCrawler): support pcre pattern
    Same as 58acaf4253e53f9c3176fe0c600c8d1f6bb717f5,
    if the pattern doesn't compile with the built-in regexp,
    we suspect the pattern has a negative lookahead -
    so we compile it with PCRE.

--- analyze.go
+++ analyze.go
@@ -397,8 +397,21 @@ func (t *Teler) checkBadCrawler(r *http.Request) error {
 
 	// Iterate over BadCrawler compiled patterns and do the check
 	for _, pattern := range t.threat.badCrawler {
+		// Initialize a variable to track whether a match is found
+		var match bool
+
+		// Check the type of the pattern
+		switch p := pattern.(type) {
+		case *regexp.Regexp: // If the pattern is a regex
+			match = p.MatchString(ua)
+		case *pcre.Matcher: // If the pattern is a PCRE expr
+			match = p.MatchString(ua, 0)
+		default: // If the pattern is of an unknown type, skip to the next iteration
+			continue
+		}
+
 		// Check if the pattern is not nil and matches the User-Agent
-		if pattern != nil && pattern.MatchString(ua) {
+		if match {
 			return errors.New("bad crawler")
 		}
 	}
