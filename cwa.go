2f2d370994db30f6916df2465b4cb2739773c3dc
Dwi Siswanto <me@dw1.io>
2023-03-12 02:09:06 +0700

    refactor: Update data types & fields in common web attack struct

--- cwa.go
+++ cwa.go
@@ -2,11 +2,11 @@ package teler
 
 type cwa struct {
 	Filters []struct {
-		Description string      `json:"description"`
-		ID          string      `json:"id"`
-		Impact      string      `json:"impact"`
-		Rule        string      `json:"rule"`
-		Tags        interface{} `json:"tags"`
+		Description string   `json:"description"`
+		ID          int64    `json:"id"`
+		Impact      int64    `json:"impact"`
+		Rule        string   `json:"rule"`
+		Tags        []string `json:"tags"`
 		pattern     interface{}
 	} `json:"filters"`
 }
