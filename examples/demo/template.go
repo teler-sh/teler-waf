bf0081f6ed47058d7e9385240b94fc75d79f837e
Dwi Siswanto <me@dw1.io>
2023-05-05 13:04:38 +0700

    update(examples/demo): rename template to 403

--- examples/demo/template.go
+++ examples/demo/template.go
@@ -3,4 +3,4 @@ package main
 import "text/template"
 
 var index = template.Must(template.ParseFiles(concat("index.html")))
-var ouch = template.Must(template.ParseFiles(concat("404.txt")))
+var ouch = template.Must(template.ParseFiles(concat("403.txt")))
