66778fd62d1ec20b1666c9fdf4dc2b1726cf60fd
Dwi Siswanto <me@dw1.io>
2024-02-25 03:20:02 +0700

    test(dsl): add should err case while env.Run
    Signed-off-by: Dwi Siswanto <me@dw1.io>

--- dsl/run_test.go
+++ dsl/run_test.go
@@ -16,10 +16,23 @@ func TestRunDSL(t *testing.T) {
 	program, err := env.Compile("1+1")
 	assert.ErrorIs(t, err, nil)
 
-	env.Requests = make(map[string]any)
+	env.Requests = map[string]any{
+		"BAR": nil,
+		"FOO": "bar",
+	}
 
 	res, err := env.Run(program)
 	assert.ErrorIs(t, err, nil)
 
 	assert.Equal(t, 2, res)
+
+	t.Run("err", func(t *testing.T) {
+		program, err := env.Compile(`"1+10O`)
+		assert.NotNil(t, err)
+
+		res, err := env.Run(program)
+		assert.NotNil(t, err)
+
+		t.Log(res)
+	})
 }
