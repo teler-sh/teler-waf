a3fa96d7c2e02ddfb0cc0bab6f02a198226beb37
Dwi Siswanto <me@dw1.io>
2024-02-25 21:44:48 +0700

    feat(threat): add license header
    Signed-off-by: Dwi Siswanto <me@dw1.io>

--- threat/verify.go
+++ threat/verify.go
@@ -1,3 +1,7 @@
+// Licensed to Dwi Siswanto under one or more agreements.
+// Dwi Siswanto licenses this file to you under the Apache 2.0 License.
+// See the LICENSE-APACHE file in the project root for more information.
+
 package threat
 
 import (
